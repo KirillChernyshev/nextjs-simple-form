45d74ee0ea8b8d541db58708e52dc6bfc45b9dc3
Kirill Chernyshev <kchernyshev@outlook.com>
2023-08-09 23:37:05 +0400

    Use SQLite as DB for prisma

---
+++ prisma/migrations/20230809193509_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Todo_title_key" ON "Todo"("title");
