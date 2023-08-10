57d5311b53ca13e90edc9c50adc789568a195885
Kirill Chernyshev <kchernyshev@outlook.com>
2023-08-10 21:13:56 +0400

    Fix error with `post` name

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,24 +1,29 @@
 import { createTodo } from "@/lib/todo.server";
 import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export const formClasses = "flex flex-col space-y-2 min-w-[50vw]";
 export const inputClasses = "rounded-md border border-gray-300 px-4 py-2";
 export const buttonClasses =
   "rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300";
 
+// This is a Server Action outside the component
+// We can use it without "use server" label
 export async function getData() {
   const todos = await prisma.todo.findMany();
-  return {
-    todos,
-  };
+  return { todos };
 }
 
 export default async function TodosRoute() {
+  // This is a Server Action inside the component
+  // We should add "use server" label
   async function addItem(formData: FormData) {
     "use server";
 
     const title = formData.get("title") as string;
-    return await createTodo({ title });
+    await createTodo({ title });
+
+    revalidatePath("/todos");
   }
 
   const { todos } = await getData();
@@ -37,7 +42,9 @@ export default async function TodosRoute() {
           </ul>
         </div>
 
-        <form className={formClasses} method="post" action={addItem}>
+        {/* Don't need to specify method="POST",
+            Next.js sends POST for form actions by default */}
+        <form key={todos.length} className={formClasses} action={addItem}>
           <input className={inputClasses} name="title" />
           <button className={buttonClasses} type="submit">
             Create Todo
